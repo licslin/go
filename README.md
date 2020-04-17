ef03f5015441f1aa19599028881c884e85486e30
licslan <licslan@sina.com>
2020-04-17 10:16:45 +0800

    start a web project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# go
+# go start basic knowledge
 learn go understand go why u go to go? who knows 
+
+# beego & bee  dev web
+beego & bee to create a web project
--- licslan_start_go/day_5_definerror/filestringserver/web.go
+++ licslan_start_go/day_5_definerror/filestringserver/web.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 func main() {
-	
+	http.HandleFunc("/list/",
+		func(writer http.ResponseWriter,
+			request *http.Request) {
+			path :=request.URL.Path[len("/list/"):]  // /list/fib.txt
+			file,err:=os.Open(path)
+			/*if err!=nil{
+				panic(err)
+			}*/
+			if err!=nil{
+				http.Error(writer,err.Error(),http.StatusInternalServerError)
+				return
+			}
+			defer file.Close()
+			//byte[]  error
+			all, err := ioutil.ReadAll(file)
+			if err!=nil{
+				panic(err)
+			}
+
+			//将byte[]写到ResponseWriter里面去
+			writer.Write(all)
+		})
+
+	fmt.Println("the server is running at port 8888----------")
+	//http server监听端口 8888
+	err:= http.ListenAndServe(":8888", nil)
+	if err!=nil{
+		panic(err)
+	}
+
 }
